598c4620abfd1591a84d4f8592f3af9a2fd919a1
sefimenko <sergey.efimenko@gmail.com>
2016-02-02 15:45:04 +0100

    deleted

---
+++ helpers.R
@@ -0,0 +1,18 @@
+# list of packages required to run RTEAnalytics.Rmd
+
+get_libraries <- function(filenames_list) { 
+  lapply(filenames_list,function(thelibrary){    
+    if (do.call(require,list(thelibrary)) == FALSE) 
+      do.call(install.packages,list(thelibrary)) 
+    do.call(library,list(thelibrary))
+  })
+}
+
+libraries_used=c("corrplot","stringr","gtools","foreign","reshape2","digest","timeDate","devtools","knitr","graphics",
+                 "grDevices","xtable","sqldf","stargazer","TTR","quantmod","shiny",
+                 "Hmisc","vegan","fpc","GPArotation","FactoMineR","cluster",
+                 "psych","stringr","googleVis", "png","ggplot2","googleVis", "gridExtra","RcppArmadillo","xts","DescTools", "dplyr")
+
+get_libraries(libraries_used)
+
+options(stringsAsFactors=FALSE)
